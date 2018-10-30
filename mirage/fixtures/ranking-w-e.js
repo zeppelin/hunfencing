4802fcdfcee153dcab90a53350a982e729cbb811
Gabor Babicz <gabor.babicz@gmail.com>
2018-10-30 21:36:00 +0100

    Install ember-fetch

---
+++ mirage/fixtures/ranking-w-e.js
@@ -0,0 +1,143 @@
+export default [
+  {
+    "rank": "1",
+    "name": "SZÁSZ-KOVÁCS Emese",
+    "club": "Vasas",
+    "birthdate": "1982-09-07",
+    "category": "felnőtt",
+    "points": "5800"
+  }, {
+    "rank": "2",
+    "name": "KUN Anna",
+    "club": "OMS-Tata",
+    "birthdate": "1995-06-25",
+    "category": "felnőtt",
+    "points": "2390"
+  }, {
+    "rank": "3",
+    "name": "BUDAI Dorina",
+    "club": "BHSE",
+    "birthdate": "1992-01-20",
+    "category": "felnőtt",
+    "points": "2210"
+  }, {
+    "rank": "4",
+    "name": "SZABÓ Réka",
+    "club": "AVA",
+    "birthdate": "1995-01-26",
+    "category": "felnőtt",
+    "points": "2070"
+  }, {
+    "rank": "5",
+    "name": "MIHÁLY Kata",
+    "club": "DHSE-PMD",
+    "birthdate": "1990-08-18",
+    "category": "felnőtt",
+    "points": "2060"
+  }, {
+    "rank": "6",
+    "name": "DÉKÁNY Kinga",
+    "club": "AVA",
+    "birthdate": "2001-09-27",
+    "category": "junior",
+    "points": "1840"
+  }, {
+    "rank": "7",
+    "name": "BÜKI Lili",
+    "club": "OMS-Tata",
+    "birthdate": "2002-05-05",
+    "category": "kadét",
+    "points": "1720",
+  }, {
+    "rank": "8",
+    "name": "BORSODY Emma",
+    "club": "BVSC",
+    "birthdate": "2000-10-02",
+    "category": "junior",
+    "points": "1580"
+  }, {
+    "rank": "9",
+    "name": "VÁRNAI Vivien",
+    "club": "BHSE",
+    "birthdate": "1994-02-12",
+    "category": "felnőtt",
+    "points": "1480"
+  }, {
+    "rank": "10",
+    "name": "LADÁNYI Nelli",
+    "club": "Szolnoki Sportiskola",
+    "birthdate": "1998-02-23",
+    "category": "felnőtt",
+    "points": "1270"
+  }, {
+    "rank": "11",
+    "name": "KARDOS Edina",
+    "club": "Vasas",
+    "birthdate": "2000-11-09",
+    "category": "junior",
+    "points": "1060"
+  }, {
+    "rank": "12",
+    "name": "NAGY Kinga",
+    "club": "DVE",
+    "birthdate": "1998-07-07",
+    "category": "felnőtt",
+    "points": "980"
+  }, {
+    "rank": "13",
+    "name": "MUHARI Eszter",
+    "club": "BHSE",
+    "birthdate": "2002-09-30",
+    "category": "kadét",
+    "points": "910"
+  }, {
+    "rank": "14",
+    "name": "GNÁM Tamara",
+    "club": "TSC",
+    "birthdate": "2000-02-15",
+    "category": "junior",
+    "points": "900"
+  }, {
+    "rank": "15",
+    "name": "PETRI Renáta",
+    "club": "AVA",
+    "birthdate": "2000-09-04",
+    "category": "junior",
+    "points": "880"
+  }, {
+    "rank": "16",
+    "name": "CSOKNYAI Lilla",
+    "club": "Vasas",
+    "birthdate": "1999-05-21",
+    "category": "junior",
+    "points": "740"
+  }, {
+    "rank": "17",
+    "name": "SZABÓ Laura",
+    "club": "BVSC",
+    "birthdate": "1993-09-27",
+    "category": "felnőtt",
+    "points": "730"
+  }, {
+    "rank": "18",
+    "name": "CSONKA Valéria",
+    "club": "Vasas",
+    "birthdate": "1996-05-30",
+    "category": "felnőtt",
+    "points": "690"
+  }, {
+    "rank": "19",
+    "name": "ÖHLMÜLLER Sarolta",
+    "club": "PTE-PEAC",
+    "birthdate": "1996-03-09",
+    "category": "felnőtt",
+    "points": "670"
+  }, {
+    "rank": "20",
+    "name": "SZABÓ Viktória",
+    "club": "Szombathely",
+    "birthdate": "1999-05-04",
+    "category": "junior",
+    "points": "640"
+  }
+];
