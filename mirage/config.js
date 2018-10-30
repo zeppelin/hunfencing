4ee941b816c540ddbbbb42b06c38c36ac8da6d6b
Gabor Babicz <gabor.babicz@gmail.com>
2018-10-30 22:22:31 +0100

    Add rankings route

--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,5 @@
+import weRankingFixtures from './fixtures/ranking-w-e';
+
 export default function() {
 
   // These comments are here to help you get started. Feel free to delete them.
@@ -9,18 +11,8 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
-  */
+  this.get('/rankings', () => weRankingFixtures);
 }
