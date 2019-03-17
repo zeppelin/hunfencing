a8b1dc84048387e1dc0cb8eed54f8c25b4a749f4
Gabor Babicz <gabor.babicz@gmail.com>
2019-03-17 15:59:06 +0100

    Rename locale en-us → en
    Resolves #10

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -1,12 +1,19 @@
 import { service } from '@ember-decorators/service';
 import Route from '@ember/routing/route';
 
+const LOCALES = ['en', 'fr', 'hu'];
+
 export default class Application extends Route {
   @service intl!: any;
   @service cookies!: unsafe;
 
   beforeModel() {
-    let savedLocale: string = this.cookies.read('locale') || 'en-us';
+    let savedLocale: string = this.cookies.read('locale');
+
+    if (!LOCALES.includes(savedLocale)) {
+      savedLocale = 'en';
+    }
+
     return this.intl.setLocale([savedLocale, 'global']);
   }
 }
