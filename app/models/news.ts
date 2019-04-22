13d43e4d977fbe6dbeac6e427fd3ce654191de11
Gabor Babicz <gabor.babicz@gmail.com>
2019-04-22 11:04:16 +0200

    [WIP]

---
+++ app/models/news.ts
@@ -0,0 +1,5 @@
+// @ts-ignore: Cannot find mdule
+import { attr, Model } from 'ember-cli-simple-store/model';
+
+export default class News extends Model {
+}
--- app/routes/index.ts
+++ app/routes/index.ts
@@ -7,19 +7,33 @@ import { makeBaseURL } from 'hunfencing/lib/api';
 
 export default class Index extends Route {
   @service fastboot!: FastBootService;
+  @service simpleStore!: any;
 
   async model() {
     if (this.fastboot.isFastBoot) {
       return;
     }
 
+    let existingRecords = this.simpleStore.find('news');
+    if (existingRecords.length >= 6) { // There are 6 news item on the home page
+      this.fetchRecords(); // Deliberately not `await`ing here
+      return existingRecords;
+    }
+
+    return this.fetchRecords();
+  }
+
+  async fetchRecords() {
     let url = makeBaseURL('news');
 
     let response = await fetch(url);
     let { data } = await response.json();
 
-    return data.map((
+    let records = data.map((
       { id, attributes }: { id: string, attributes: Dict<unknown> }
     ) => ({ id, ...attributes }));
+
+    this.simpleStore.pushArray('news', records);
+    return this.simpleStore.find('news');
   }
 }
